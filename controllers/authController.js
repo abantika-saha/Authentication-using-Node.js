6beca2fe9a477ce78363ab587bb4c77e45319f29
Abantika Saha <65250567+abantika-saha@users.noreply.github.com>
2021-06-27 14:07:28 +0530

    Add files via upload

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require('jsonwebtoken');
 
 //handle errors
 const handleErrors = (err) => {
@@ -19,6 +20,13 @@ if(err.code === 11000){
     }
     return errors;
 }
+
+const maxAge = 3*24*60*60; //3 days in seconds because jwt expects time in seconds, cookie in millisec
+const createToken = (id) => {
+    return jwt.sign({ id }, 'aban secret', {
+        expiresIn: maxAge 
+    });
+}
 //to store handler func
 module.exports.signup_get = (req,res) =>{
     res.render('signup');
@@ -33,7 +41,9 @@ module.exports.signup_post = async (req,res) =>{
     
     try{
         const user = await User.create({email,password});
-        res.status(201).json(user);        
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge*1000 });
+        res.status(201).json({user: user._id});        
     }
     catch(err){
         const errors = handleErrors(err);
@@ -44,7 +54,16 @@ module.exports.signup_post = async (req,res) =>{
 module.exports.login_post = async (req,res) =>{
     // console.log(req.body);
     const {email, password} = req.body;
-    console.log(email,password);
-    res.send('user login');
+
+    // console.log(email,password);
+    // res.send('user login');
+
+    try{
+        const user = await User.login(email,password);
+        res.status(200).json({user: user._id})
+    }
+    catch(err){
+        res.status(400).json({});
+    }
 }
 
