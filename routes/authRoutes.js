9be17ba01e3a9591276da80c1f9050f3bc9e295b
Abantika Saha <65250567+abantika-saha@users.noreply.github.com>
2021-06-28 12:17:48 +0530

    Add files via upload

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,7 @@ router.get('/signup',authController.signup_get);
 router.post('/signup',authController.signup_post);
 router.get('/login',authController.login_get);
 router.post('/login',authController.login_post);
+router.get('/logout',authController.logout_get);
 
 //export router to import in app.js 
 module.exports=router;
