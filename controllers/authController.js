e194c7a4b6f40f2e549f64ce923c213be4bfa177
Abantika Saha <65250567+abantika-saha@users.noreply.github.com>
2021-06-28 12:17:04 +0530

    Add files via upload

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,69 +1,91 @@
 const User = require("../models/User");
 const jwt = require('jsonwebtoken');
 
-//handle errors
+// handle errors
 const handleErrors = (err) => {
-    console.log(err.message, err.code); //error code works only if the email not unique for most cases it is undefined
-    let errors = { email: '', password: '' };
+  console.log(err.message, err.code);
+  let errors = { email: '', password: '' };
 
-//duplicate error code
-if(err.code === 11000){
+  // incorrect email
+  if (err.message === 'incorrect email') {
+    errors.email = 'The email is not registered';
+  }
+
+  // incorrect password
+  if (err.message === 'incorrect password') {
+    errors.password = 'The password is incorrect';
+  }
+
+  // duplicate email error
+  if (err.code === 11000) {
     errors.email = 'The email is already registered';
     return errors;
-}
+  }
 
-    //validation errors
-    if(err.message.includes('user validation failed')){
-        Object.values(err.errors).forEach(({ properties }) =>{
-            errors[properties.path] = properties.message;
-        })
-    }
-    return errors;
-}
-
-const maxAge = 3*24*60*60; //3 days in seconds because jwt expects time in seconds, cookie in millisec
-const createToken = (id) => {
-    return jwt.sign({ id }, 'aban secret', {
-        expiresIn: maxAge 
+  // validation errors
+  if (err.message.includes('user validation failed')) {
+    // console.log(err);
+    Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(val);
+      // console.log(properties);
+      errors[properties.path] = properties.message;
     });
-}
-//to store handler func
-module.exports.signup_get = (req,res) =>{
-    res.render('signup');
-}
+  }
 
-module.exports.login_get = (req,res) =>{
-    res.render('login');
+  return errors;
 }
 
-module.exports.signup_post = async (req,res) =>{
-    const {email, password} = req.body;
-    
-    try{
-        const user = await User.create({email,password});
-        const token = createToken(user._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge*1000 });
-        res.status(201).json({user: user._id});        
-    }
-    catch(err){
-        const errors = handleErrors(err);
-        res.status(400).json({errors});
-    }
+// create json web token
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, 'aban secret', {
+    expiresIn: maxAge
+  });
+};
+
+// controller actions
+module.exports.signup_get = (req, res) => {
+  res.render('signup');
 }
 
-module.exports.login_post = async (req,res) =>{
-    // console.log(req.body);
-    const {email, password} = req.body;
-
-    // console.log(email,password);
-    // res.send('user login');
-
-    try{
-        const user = await User.login(email,password);
-        res.status(200).json({user: user._id})
-    }
-    catch(err){
-        res.status(400).json({});
-    }
+module.exports.login_get = (req, res) => {
+  res.render('login');
 }
 
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password });
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
+  }
+  catch(err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+ 
+}
+
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id });
+  } 
+  catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+
+}
+
+module.exports.logout_get = (req,res) =>{
+    //replace jwt with a blank cookie with a very short expiry date
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
+}
